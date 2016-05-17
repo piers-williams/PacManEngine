a1f3617d590712b0b465842241ac11ed429c8874
Piers Williams <solar_1992@hotmail.co.uk>
2016-05-17 11:45:20 +0100

    Added test to see if faster is same as slower

--- src/main/java/pacman/test/GhostPredictionsTest.java
+++ src/main/java/pacman/test/GhostPredictionsTest.java
@@ -1,6 +1,7 @@
 package pacman.test;
 
 import pacman.controllers.examples.po.mcts.prediction.GhostPredictions;
+import pacman.controllers.examples.po.mcts.prediction.fast.GhostPredictionsFast;
 import pacman.game.Constants;
 import pacman.game.Game;
 import pacman.game.comms.BasicMessenger;
@@ -14,12 +15,17 @@ public class GhostPredictionsTest {
         Game game = new Game(System.currentTimeMillis(), new BasicMessenger(0, 1, 1));
 
         GhostPredictions predictor = new GhostPredictions(game.getCurrentMaze());
-
+        GhostPredictionsFast predictionsFast = new GhostPredictionsFast(game.getCurrentMaze());
         predictor.observe(Constants.GHOST.INKY, 6, game.getPossibleMoves(6)[0]);
+        predictionsFast.observe(Constants.GHOST.INKY, 6, game.getPossibleMoves(6)[0]);
+
 
         for(int i = 0; i < 100; i++){
             predictor.update();
+            predictionsFast.update();
             System.out.println(predictor.getGhostInfo(Constants.GHOST.INKY));
+            System.out.println(predictionsFast.getGhostInfo(Constants.GHOST.INKY));
+
         }
         System.out.println(predictor.sampleLocations());
         System.out.println(predictor.sampleLocations());
@@ -29,12 +35,19 @@ public class GhostPredictionsTest {
         System.out.println("\n\n\n Observing unseen things");
 
         predictor = new GhostPredictions(game.getCurrentMaze());
+        predictionsFast = new GhostPredictionsFast(game.getCurrentMaze());
         predictor.observe(Constants.GHOST.INKY, 165, game.getPossibleMoves(165)[0]);
+        predictionsFast.observe(Constants.GHOST.INKY, 165, game.getPossibleMoves(165)[0]);
         System.out.println(predictor.getGhostInfo(Constants.GHOST.INKY));
+        System.out.println(predictionsFast.getGhostInfo(Constants.GHOST.INKY));
         predictor.update();
+        predictionsFast.update();
         System.out.println(predictor.getGhostInfo(Constants.GHOST.INKY));
+        System.out.println(predictionsFast.getGhostInfo(Constants.GHOST.INKY));
         predictor.observeNotPresent(Constants.GHOST.INKY, 166);
+        predictionsFast.observeNotPresent(Constants.GHOST.INKY, 166);
         System.out.println(predictor.getGhostInfo(Constants.GHOST.INKY));
+        System.out.println(predictionsFast.getGhostInfo(Constants.GHOST.INKY));
 
 
         System.out.println(predictor.sampleLocations());
